32c50884c0b69e8a0ec09cf04ed0119f9e46923f
Danie Viljoen <danie.viljoen@orionhealth.com>
2016-10-04 20:24:27 +1300

    cleanup messages

--- js/Messages.js
+++ js/Messages.js
@@ -1,34 +1,9 @@
 
 var Messages = {
   SendMsg: "topic_SendingMsg-send",
-  
-  
   MsgDisplay: "topic_MsgDisplay",
-  MsgDisplay_open: "topic_MsgDisplay-open",
-  MsgDisplay_display: "topic_MsgDisplay-display",
-  MsgDisplay_hide: "topic_MsgDisplay-hide",
-  
-  MsgView: "topic_MsgView",
-  MsgViewOpened: "topic_MsgView-msgOpened",
-  
   MsgSave: "topic_MsgSave",
-  MsgSave_save: "topic_MsgSave_save",
-  
-  //sending msg
   SendingMsg: "SendingMsg",
-  SendingMsg_start: "SendingMsg.start",
-  SendingMsg_cancel: "SendingMsg.cancel",
-  
-  
-  //view messages
-  
-  MsgSender: "MsgSender",
-  MsgSender_open: "MsgSender.open",
-  
   MenuBar: "topic_MenuBar",
-  MsgSaved: "topic_MenuBar-msgSaved",
-  MsgOpened: "topic_MenuBar-msgOpened",
-  MsgSavedRequired: "topic_MenuBar-msgSavedReq",
   MsgSend: "topic_MenuBar-msgSend"
-  
 };
